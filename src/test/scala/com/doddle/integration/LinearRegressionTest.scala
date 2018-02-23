128f00de62cd0ff8c15011aca6228d6e25c31d09
inejc <nejc.ilenic@gmail.com>
2018-02-23 11:52:00 +0100

    add rmse test

--- src/test/scala/com/doddle/integration/LinearRegressionTest.scala
+++ src/test/scala/com/doddle/integration/LinearRegressionTest.scala
@@ -8,11 +8,12 @@ import org.scalatest.{FlatSpec, Matchers}
 class LinearRegressionTest extends FlatSpec with Matchers {
 
   "LinearRegression" should "achieve a resonable score on the Boston housing dataset" in {
-    // todo: evaluate on test set
     val (x, y) = loadBoston
     val model = LinearRegression()
+
     model.fit(x, y)
     val yPred = model.predict(x)
+
     rmse(y, yPred) should be < 5.0
   }
 }
--- src/test/scala/com/doddle/metrics/RegressionMetricsTest.scala
+++ src/test/scala/com/doddle/metrics/RegressionMetricsTest.scala
@@ -1,10 +1,15 @@
 package com.doddle.metrics
 
+import breeze.linalg.DenseVector
+import com.doddle.metrics.RegressionMetrics.rmse
 import org.scalatest.{FlatSpec, Matchers}
 
 class RegressionMetricsTest extends FlatSpec with Matchers {
 
-  "RegressionMetrics" should "todo" in {
-    // todo
+  "RegressionMetrics" should "calculate a correct rmse value" in {
+    val y = DenseVector(1.0, 4.1, 2.2, 5.1, 9.6)
+    val yPred = DenseVector(1.2, 1.4, 8.2, 3.1, 9.6)
+
+    rmse(y, yPred) shouldEqual 3.076686529368892
   }
 }
