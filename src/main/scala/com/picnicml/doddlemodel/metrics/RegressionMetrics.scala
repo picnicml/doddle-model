ba2fee2e083cbdd1e611d935ddbcd31683a27eea
Evan Haldane <evanhaldane@gmail.com>
2018-05-09 18:26:32 -0400

    add some regression metrics

--- src/main/scala/com/picnicml/doddlemodel/metrics/RegressionMetrics.scala
+++ src/main/scala/com/picnicml/doddlemodel/metrics/RegressionMetrics.scala
@@ -1,6 +1,8 @@
 package com.picnicml.doddlemodel.metrics
 
-import breeze.numerics.sqrt
+import breeze.numerics._
+import breeze.linalg._
+import breeze.stats.variance
 import com.picnicml.doddlemodel.data.Target
 
 object RegressionMetrics {
@@ -13,6 +15,29 @@ object RegressionMetrics {
     override def apply(y: Target, yPred: Target): Double = {
       val d = y - yPred
       sqrt((d.t * d) / y.length.toDouble)
+    }
+  }
+
+  /** Mean absolute error */
+  object Mae extends Metric {
+
+    lazy val higherValueIsBetter: Boolean = false
+
+    override def apply(y: Target, yPred: Target): Double = {
+      val d = y - yPred
+      sum(abs(d)) / y.length.toDouble
+    }
+  }
+
+
+  /** Explained variance */
+  object ExplainedVariance extends Metric {
+
+    lazy val higherValueIsBetter: Boolean = true
+
+    override def apply(y: Target, yPred: Target): Double = {
+      val d = y - yPred
+      1 - variance(d)/variance(y)
     }
   }
 }
