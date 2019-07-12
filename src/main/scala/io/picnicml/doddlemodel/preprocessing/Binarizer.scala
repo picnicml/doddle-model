8b78e30dc5ad0def02a67d277f24d01b6a79aa00
Matej Klemen <matej.klemen1337@gmail.com>
2019-07-12 18:35:41 +0200

    Fix Binarizer's compilation issue * swap argument order in private case class to prevent conflicts between auto-generated apply method and custom apply method that is exposed to the public

--- src/main/scala/io/picnicml/doddlemodel/preprocessing/Binarizer.scala
+++ src/main/scala/io/picnicml/doddlemodel/preprocessing/Binarizer.scala
@@ -5,8 +5,8 @@ import io.picnicml.doddlemodel.data.Feature.FeatureIndex
 import io.picnicml.doddlemodel.data.{Features, RealVector}
 import io.picnicml.doddlemodel.typeclasses.Transformer
 
-case class Binarizer private (private val thresholds: RealVector,
-                              private val featureIndex: FeatureIndex)
+case class Binarizer private (private val featureIndex: FeatureIndex,
+                              private val thresholds: RealVector)
 
 object Binarizer {
 
@@ -14,14 +14,14 @@ object Binarizer {
     val numNumeric: Int = featureIndex.numerical.columnIndices.length
     require(numNumeric > 0, "There must be at least 1 numeric column in the given data")
     val thresholdsExtended = DenseVector.fill(numNumeric) {threshold}
-    new Binarizer(thresholdsExtended, featureIndex)
+    new Binarizer(featureIndex, thresholdsExtended)
   }
 
   def apply(thresholds: RealVector, featureIndex: FeatureIndex): Binarizer = {
     val numNumeric = featureIndex.numerical.columnIndices.length
     require(numNumeric > 0, "There must be at least 1 numeric column in the given data")
     require(numNumeric == thresholds.length, "A threshold should be given for every numerical column")
-    new Binarizer(thresholds, featureIndex)
+    new Binarizer(featureIndex, thresholds)
   }
 
   implicit lazy val ev: Transformer[Binarizer] = new Transformer[Binarizer] {
