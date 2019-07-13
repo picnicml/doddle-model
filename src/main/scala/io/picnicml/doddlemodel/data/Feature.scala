7b3a30bb555e4ae32813d04de884ad14498d8c15
Matej Klemen <matej.klemen1337@gmail.com>
2019-07-13 12:39:13 +0200

    Update docs for Feature (FeatureIndex's functions)

--- src/main/scala/io/picnicml/doddlemodel/data/Feature.scala
+++ src/main/scala/io/picnicml/doddlemodel/data/Feature.scala
@@ -35,11 +35,33 @@ object Feature {
       subset(subsetIndices:_*)
     }
 
+    /** Create a feature index with subset of features, provided by feature names.
+      * @param names subset of features to be selected
+      *
+      * @example Create feature index based on features "f1" and "f3" from a constructed feature index.
+      *   {{{
+      *     val featureIndex = FeatureIndex(List("f1", "f2", "f3"), List(NumericalFeature, NumericalFeature,
+      *       NumericalFeature), List(0, 1, 2))
+      *     val subIndex = featureIndex.subset("f1", "f3")
+      *   }}}
+      *
+      * */
     def subset(names: String*): FeatureIndex = {
       val nameToIndex = this.names.zipWithIndex.toMap
       subset(names.map(n => nameToIndex(n)):_*)
     }
 
+    /** Create a feature index with subset of features, provided by feature indices.
+      * @param indices column indices for subset of features to be selected
+      *
+      * @example Create feature index based on second and third (i.e. indices 1, 2) features from a constructed
+      *          feature index.
+      * {{{
+      *   val featureIndex = FeatureIndex(List("f1", "f2", "f3"), List(NumericalFeature, NumericalFeature,
+      *     NumericalFeature), List(0, 1, 2))
+      *   val subIndex = featureIndex.subset(1 to 2)
+      * }}}
+      * */
     def subset(indices: IndexedSeq[Int]): FeatureIndex = subset(indices:_*)
 
     // DummyImplicit is needed to avoid the same type as String* after erasure
@@ -49,6 +71,7 @@ object Feature {
       indices.toIndexedSeq.map(i => this.columnIndices(i))
     )
 
+    /** Create a feature index by dropping a feature by column index. */
     def drop(index: Int): FeatureIndex = new FeatureIndex(
       this.names.zipWithIndex.flatMap { case (n, i) => if (i != index) n.some else none[String] },
       this.types.zipWithIndex.flatMap { case (t, i) => if (i != index) t.some else none[FeatureType] },
@@ -61,26 +84,54 @@ object Feature {
       this.names.zip(this.types).map { case (n, t) => s"$n (${t.headerLineString})" } mkString ", "
   }
 
+  /** A structure that keeps track of feature metadata (names, types and indices). This is needed
+    * because some methods are only applicable to a certain type of features, e.g. [0, 1] scaling
+    * only makes sense for numerical features. */
   object FeatureIndex {
-
+    /** Construct feature index with `n` categorical features. Feature names are generated automatically.
+      * @param n number of categorical features in feature index
+      */
     def categorical(n: Int): FeatureIndex =
       categorical((0 until n).toList)
 
     def categorical(columnIndices: List[Int]): FeatureIndex =
       apply(columnIndices.indices.map(i => s"f$i").toList, columnIndices.map(_ => CategoricalFeature), columnIndices)
 
+    /** Construct feature index with `n` numerical features. Feature names are generated automatically.
+      * @param n number of numerical features in feature index
+      */
     def numerical(n: Int): FeatureIndex =
       numerical((0 until n).toList)
 
     def numerical(columnIndices: List[Int]): FeatureIndex =
       apply(columnIndices.indices.map(i => s"f$i").toList, columnIndices.map(_ => NumericalFeature), columnIndices)
 
+    /** Construct feature index from feature types. Feature names are generated automatically.
+      * @param types list of feature types
+      *
+      * @example Construct a feature index with one numerical and two categorical features.
+      *   {{{
+      *     val featureIndex = FeatureIndex(List(CategoricalFeature, NumericalFeature, CategoricalFeature))
+      *   }}}
+      */
     def apply(types: List[FeatureType]): FeatureIndex =
       apply(types.indices.map(i => s"f$i").toList, types, types.indices.toList)
 
     def apply(types: List[FeatureType], columnIndices: List[Int]): FeatureIndex =
       apply(types.indices.map(i => s"f$i").toList, types, columnIndices)
 
+    /** Construct a feature index with custom feature names, types and column indices.
+      * @param names feature names
+      * @param types feature types
+      * @param columnIndices column index for each feature
+      *
+      * @example Construct a feature index with three features, named "age" (numerical), "height" (numerical)
+      *          and "group" (categorical).
+      *   {{{
+      *     val featureIndex = FeatureIndex(List("age", "height", "group"), List(NumericalFeature,
+      *       NumericalFeature, CategoricalFeature), List(0, 1, 2))
+      *   }}}
+      */
     def apply(names: List[String], types: List[FeatureType], columnIndices: List[Int]): FeatureIndex =
       new FeatureIndex(names.toIndexedSeq, types.toIndexedSeq, columnIndices.toIndexedSeq)
   }
