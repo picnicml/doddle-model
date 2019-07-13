14c588d3dafb50220515391eaa643a8af2bdabe0
Matej Klemen <matej.klemen1337@gmail.com>
2019-07-13 12:37:33 +0200

    Update docs for DatasetUtils (shuffleDataset, splitDataset)

--- src/main/scala/io/picnicml/doddlemodel/data/DatasetUtils.scala
+++ src/main/scala/io/picnicml/doddlemodel/data/DatasetUtils.scala
@@ -6,13 +6,47 @@ import scala.util.Random
 
 object DatasetUtils {
 
-  /** Shuffles rows of the dataset. */
+  /** Shuffles rows of the dataset.
+    * @param x features to be shuffled
+    * @param y labels corresponding to features
+    *
+    * @example Shuffle a dataset randomly.
+    *   {{{
+    *     import scala.util.Random
+    *
+    *     // we are assuming data was previously loaded
+    *     val (dataX, dataY) = ...
+    *
+    *     val (shuffledX, shuffledY) = shuffleDataset(dataX, dataY)
+    *
+    *     // seeded shuffle - seed passed to shuffler implicitly
+    *     implicit val rand: Random = new Random(42)
+    *     val (shuffledX, shuffledY) = shuffleDataset(dataX, dataY)
+    *   }}}
+    * */
   def shuffleDataset(x: Features, y: Target)(implicit rand: Random = new Random()): Dataset = {
     val shuffleIndices = rand.shuffle((0 until y.length).toIndexedSeq)
     (x(shuffleIndices, ::).toDenseMatrix, y(shuffleIndices).toDenseVector)
   }
 
-  /** Splits the dataset into two subsets for training and testing. */
+  /** Splits the dataset into two subsets for training and testing.
+    * @param x features to be split
+    * @param y labels corresponding to features
+    * @param proportionTrain proportion of dataset to be put into training set - between 0.0 and 1.0
+    *
+    * @example Split dataset into training and test set.
+    *   {{{
+    *     // we are assuming data was previously loaded
+    *     val (dataX, dataY) = ...
+    *
+    *     // by default, the split is 50%:50%
+    *     val trTeSplit = splitDataset(dataX, dataY)
+    *
+    *     // put 80% of data into training set and 20% into test set
+    *     val trTeSplit = splitDataset(dataX, dataY, 0.8)
+    *     val (trainX, trainY, testX, testY) = (trTeSplit.xTr, trTeSplit.yTr, trTeSplit.xTe, trTeSplit.yTe)
+    *   }}}
+    * */
   def splitDataset(x: Features, y: Target, proportionTrain: Float = 0.5f): TrainTestSplit = {
     val numTrain = numberOfTrainExamplesBasedOnProportion(x.rows, proportionTrain)
     val trIndices = 0 until numTrain
