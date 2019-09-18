b912b0d539a03187c3361d10a087e4cbe14fccba
Wei Chen <weichen@apache.org>
2019-09-18 10:13:07 +0800

    minSamples-bug-fix

--- src/main/scala/io/picnicml/doddlemodel/cluster/DBSCAN.scala
+++ src/main/scala/io/picnicml/doddlemodel/cluster/DBSCAN.scala
@@ -39,47 +39,39 @@ object DBSCAN {
       model.copy(label = label.some)
 
     override protected def fitSafe(model: DBSCAN, x: Features): DBSCAN = {
-      val xSize = x.rows
-      val label = Array.fill[Int](xSize)(Int.MaxValue)
-      var groupStartId = 0
-      var groupId = 0
-      while (groupStartId < xSize) {
-        var groupQueue = Set(groupStartId)
-        var groupCount = 1
-        label(groupStartId) = groupId
-        while (groupQueue.size > 0) {
-          val tmpGroupQueue = groupQueue
-          groupQueue = Set[Int]()
-          for (pointId <- tmpGroupQueue; i <- 0 until xSize
-            if label(i) > groupId &&
-              euclideanDistance(x(i, ::).t, x(pointId, ::).t) <= model.eps) {
-            label(i) = groupId
-            groupQueue += i
-            groupCount += 1
+      val label = Array.fill[Int](x.rows)(Int.MaxValue)
+      var groupId = -1
+      for (pointId <- 0 until x.rows if label(pointId) == Int.MaxValue) {
+        var groupQueue = findNeighbors(pointId, x, model.eps)
+        if (groupQueue.size + 1 < model.minSamples) {
+          label(pointId) = -1
+        } else {
+          groupId += 1
+          label(pointId) = groupId
+          while (groupQueue.size > 0) {
+            val tmpGroupQueue = groupQueue
+            groupQueue = Set[Int]()
+            tmpGroupQueue.foreach { i =>
+              if (label(i) == -1) label(i) = groupId
+              else if (label(i) == Int.MaxValue) {
+                label(i) = groupId
+                val neighbors = findNeighbors(i, x, model.eps)
+                if (neighbors.size + 1 < model.minSamples)
+                  groupQueue ++= neighbors
+              }
+            }
           }
         }
-        if (groupCount < model.minSamples) {
-          for (i <- 0 until xSize if label(i) == groupId) {
-            label(i) = -1
-          }
-          groupId -= 1
-        }
-        groupStartId = findUnidentifiedPoint(label)
-        groupId += 1
       }
       copy(model, label)
     }
 
-    override protected def fitPredictSafe(model: DBSCAN, x: Features): Array[Int] = {
+    override protected def fitPredictSafe(model: DBSCAN, x: Features): Array[Int] =
       labelSafe(fitSafe(model, x))
-    }
 
-    private def findUnidentifiedPoint(label: Array[Int]): Int = {
-      var i = 0
-      while (i < label.length && label(i) < Int.MaxValue) {
-        i += 1
-      }
-      i
-    }
+    private def findNeighbors(pointId: Int, x: Features, eps: Double): Set[Int] =
+      (0 until x.rows).filter { i =>
+        i != pointId && euclideanDistance(x(i, ::).t, x(pointId, ::).t) <= eps
+      }.toSet
   }
 }
