d6bfb857ed8eb0e10b09927aaa801a970f1f4e95
Helw150 <wbh230@nyu.edu>
2019-09-21 14:18:53 -0700

    Use Breeze Descriptive Stats and Ranges instead of clean buckets

--- src/main/scala/io/picnicml/doddlemodel/preprocessing/QuantileDiscretizer.scala
+++ src/main/scala/io/picnicml/doddlemodel/preprocessing/QuantileDiscretizer.scala
@@ -1,14 +1,17 @@
 package io.picnicml.doddlemodel.preprocessing
 
 import breeze.linalg.DenseVector
+import breeze.stats.DescriptiveStats
 import io.picnicml.doddlemodel.CrossScalaCompat._
 import io.picnicml.doddlemodel.data.Feature.FeatureIndex
-import io.picnicml.doddlemodel.data.{Features, RealVector}
+import io.picnicml.doddlemodel.data.{Features}
 import io.picnicml.doddlemodel.typeclasses.Transformer
+import scala.Double.{MinValue, MaxValue}
 
-case class QuantileDiscretizer(private val numQuantiles: RealVector, private val featureIndex: FeatureIndex) {
+case class QuantileDiscretizer(private val bucketCounts: DenseVector[Double], private val featureIndex: FeatureIndex) {
   private val numNumeric = featureIndex.numerical.columnIndices.length
-  require(numNumeric == 0 || numNumeric == numQuantiles.length, "A quantile should be given for every numerical column")
+  var splitPointCache = Option.empty
+  require(numNumeric == 0 || numNumeric == bucketCounts.length, "A quantile should be given for every numerical column")
 }
 
 /** An immutable preprocessor that discretizes numerical features into buckets based on quantiles.
@@ -16,13 +19,13 @@ case class QuantileDiscretizer(private val numQuantiles: RealVector, private val
   * */
 object QuantileDiscretizer {
 
-  /** Create a quantile discretizer where data is split into quartiles.
+  /** Create a quantile discretizer which splits data into discrete evenly sized buckets.
     *
-    * @param numQuantiles The number of quantiles desired
+    * @param bucketCounts The number of quantiles desired
     * @param featureIndex feature index associated with features - this is needed so that only numerical features are
     *                     transformed by this preprocessor; could be a subset of columns to be transformed
     *
-    * @example Quantile Discretize a matrix with two features: one numerical and one categorical.
+    * @example Discretize a matrix into quartiles with two features: one numerical and one categorical.
     *   {{{
     *     import io.picnicml.doddlemodel.preprocessing.QuantileDiscretizer.ev
     *
@@ -34,48 +37,67 @@ object QuantileDiscretizer {
     *       List(5.0, 0.0)
     *     )
     *     // equivalently, DenseVector(4) could be used
-    *     val numQuantiles = 4
-    *     val discretizer = QuantileDiscretizer(numQuantiles, featureIndex)
+    *     val bucketCounts = 4
+    *     val discretizer = QuantileDiscretizer(bucketCounts, featureIndex)
     *     // Note: no fitting required
     *     val xTransformed = ev.transform(discretizer, x)
     *   }}}
     */
-  def apply(numQuantiles: Int, featureIndex: FeatureIndex): QuantileDiscretizer = {
+  def apply(bucketCount: Int, featureIndex: FeatureIndex): QuantileDiscretizer = {
     val numNumeric = featureIndex.numerical.columnIndices.length
-    val numQuantilesExtended: DenseVector[Double] = DenseVector.fill(numNumeric) { numQuantiles.toDouble }
-    QuantileDiscretizer(numQuantilesExtended, featureIndex)
-  }
-
-  def splitEvenly[A](target: Seq[A], numQuantiles: RealVector, index: Int): Seq[Seq[A]] = {
-    val numBuckets = numQuantiles(index).toInt
-    val length = target.size
-    val baseBucketSize =  length / numBuckets
-    val remainder = length % numBuckets
-    val sizeSplitPoint = length - remainder * (baseBucketSize + 1)
-    val (smallerBuckets, biggerBuckets) = target.splitAt(sizeSplitPoint)
-    val bucketIterator = smallerBuckets.grouped(baseBucketSize) ++ biggerBuckets.grouped((baseBucketSize + 1))
-    bucketIterator.toSeq
+    val bucketCountExtended: DenseVector[Double] = DenseVector.fill(numNumeric) { bucketCount.toDouble }
+    QuantileDiscretizer(bucketCountExtended, featureIndex)
   }
 
   implicit lazy val ev: Transformer[QuantileDiscretizer] = new Transformer[QuantileDiscretizer] {
 
+
+
     override def isFitted(model: QuantileDiscretizer): Boolean = true
 
-    override def fit(model: QuantileDiscretizer, x: Features): QuantileDiscretizer = model
+    override def fit(model: QuantileDiscretizer, x: Features): QuantileDiscretizer = {
+      val xCopy = x.copy
+      val discreteRangeArrays: Seq[Seq[(Double, Double)]] = model.featureIndex.numerical.columnIndices.zipWithIndex.map {
+        case (colIndex, bucketCountsIndex) =>
+          val colArray = xCopy(::, colIndex).toScalaVector.sorted
+          computeQuantiles(colArray, model.bucketCounts, bucketCountsIndex)
+      }
+      println(discreteRangeArrays)
+      model
+    }
 
     override protected def transformSafe(model: QuantileDiscretizer, x: Features): Features = {
       val xCopy = x.copy
       model.featureIndex.numerical.columnIndices.zipWithIndex.foreach {
-        case (colIndex, numQuantilesIndex) =>
+        case (colIndex, bucketCountsIndex) =>
           val colArray = xCopy(::, colIndex).toScalaVector.sorted
-          val buckets = splitEvenly(colArray, model.numQuantiles, numQuantilesIndex)
+          val buckets = computeQuantiles(colArray, model.bucketCounts, bucketCountsIndex)
           (0 until xCopy.rows).foreach {
             rowIndex => xCopy(rowIndex, colIndex) = buckets
-              .indexWhere(_.contains(xCopy(rowIndex, colIndex)))
+              .indexWhere({
+                case (lowerBound, upperBound) => lowerBound <= xCopy(rowIndex, colIndex) && xCopy(rowIndex, colIndex) <= upperBound
+              })
               .toDouble
           }
       }
       xCopy
     }
+  }
+
+  private def computeQuantiles(target: Seq[Double], bucketCounts: DenseVector[Double], index: Int): Seq[(Double, Double)] = {
+    val bucketCount = bucketCounts(index).toInt
+    val binPercentileWidth = 1.0 / bucketCount
+    val targetArray = target.toArray
+    // NOTE: Adds binPercentileWidth to make the range inclusive of 1
+    val percentileRangePairs = Range.BigDecimal(0, 1.0+(binPercentileWidth/2), binPercentileWidth).map(_.toDouble).sliding(2).toSeq
+    val rangePairs = percentileRangePairs.map {
+      case Seq(lowerBound, upperBound) =>
+        (DescriptiveStats.percentileInPlace(targetArray, lowerBound), DescriptiveStats.percentileInPlace(targetArray, upperBound))
+    }
+    val headUpdate = rangePairs.head.copy(_1 = MinValue)
+    val lastUpdate = rangePairs.last.copy(_2 = MaxValue)
+    rangePairs
+      .updated(0, headUpdate)
+      .updated(rangePairs.size-1, lastUpdate)
   }
 }
--- src/test/scala/io/picnicml/doddlemodel/preprocessing/QuantileDiscretizerTest.scala
+++ src/test/scala/io/picnicml/doddlemodel/preprocessing/QuantileDiscretizerTest.scala
@@ -15,26 +15,26 @@ class QuantileDiscretizerTest extends FlatSpec with Matchers with TestingUtils {
     List(5.0, 20.0, 0.0)
   )
 
-  "QuantileDiscretizer" should "bucket all numerical features into the number of buckets defined in numQuantiles" in {
+  "QuantileDiscretizer" should "bucket all numerical features into the number of buckets defined in bucketCounts" in {
     val featureIndex = FeatureIndex(List(NumericalFeature, NumericalFeature, CategoricalFeature))
-    val numQuantiles: DenseVector[Double] = DenseVector(3, 4)
+    val bucketCounts: DenseVector[Double] = DenseVector(3, 4)
 
-    val quantileDiscretizer = QuantileDiscretizer(numQuantiles, featureIndex)
+    val quantileDiscretizer = QuantileDiscretizer(bucketCounts, featureIndex)
     val xQuantizedExpected = DenseMatrix(
       List(0.0, 0.0, 0.0),
       List(1.0, 1.0, 1.0),
-      List(2.0, 3.0, 0.0),
+      List(1.0, 3.0, 0.0),
       List(2.0, 2.0, 0.0)
     )
 
     breezeEqual(ev.transform(quantileDiscretizer, x), xQuantizedExpected) shouldBe true
   }
 
-  it should "process all the numerical columns by a single numQuantiles" in {
+  it should "process all the numerical columns by a single bucketCounts" in {
     val featureIndex = FeatureIndex(List(NumericalFeature, NumericalFeature, NumericalFeature))
-    val numQuantiles: Int = 2
+    val bucketCount: Int = 2
 
-    val quantileDiscretizer = QuantileDiscretizer(numQuantiles, featureIndex)
+    val quantileDiscretizer = QuantileDiscretizer(bucketCount, featureIndex)
     val xQuantizedExpected = DenseMatrix(
       List(0.0, 0.0, 0.0),
       List(0.0, 0.0, 1.0),
@@ -47,21 +47,21 @@ class QuantileDiscretizerTest extends FlatSpec with Matchers with TestingUtils {
 
   it should "amount to no-op if there are no numerical features in data" in {
     val featureIndex = FeatureIndex(List(CategoricalFeature, CategoricalFeature, CategoricalFeature))
-    val numQuantiles1: DenseVector[Double] = DenseVector(2, 3)
-    val numQuantiles2: Int = 3
+    val bucketCounts: DenseVector[Double] = DenseVector(2, 3)
+    val bucketCount: Int = 3
 
-    val quantileDiscretizer1 = QuantileDiscretizer(numQuantiles1, featureIndex)
-    val quantileDiscretizer2 = QuantileDiscretizer(numQuantiles2, featureIndex)
+    val quantileDiscretizer1 = QuantileDiscretizer(bucketCounts, featureIndex)
+    val quantileDiscretizer2 = QuantileDiscretizer(bucketCount, featureIndex)
 
     breezeEqual(ev.transform(quantileDiscretizer1, x), x) shouldBe true
     breezeEqual(ev.transform(quantileDiscretizer2, x), x) shouldBe true
   }
 
-  it should "fail when the amount of passed numQuantiles is different to number of numerical features in data" in {
+  it should "fail when the amount of passed bucketCounts is different to number of numerical features in data" in {
     val featureIndex = FeatureIndex(List(NumericalFeature, NumericalFeature, NumericalFeature))
-    val numQuantiles: DenseVector[Double] = DenseVector(2, 3)
+    val bucketCounts: DenseVector[Double] = DenseVector(2, 3)
 
     // 3 numeric columns vs 2 thresholds
-    an [IllegalArgumentException] should be thrownBy QuantileDiscretizer(numQuantiles, featureIndex)
+    an [IllegalArgumentException] should be thrownBy QuantileDiscretizer(bucketCounts, featureIndex)
   }
 }
