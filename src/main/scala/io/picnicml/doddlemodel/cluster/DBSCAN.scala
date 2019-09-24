65204ef742f2ecc6d9d3c73ba7c63a71d9580172
inejc <nejc.ilenic@gmail.com>
2019-09-24 18:46:41 +0200

    start working on DBSCAN by @Wei-1

--- src/main/scala/io/picnicml/doddlemodel/cluster/DBSCAN.scala
+++ src/main/scala/io/picnicml/doddlemodel/cluster/DBSCAN.scala
@@ -1,90 +1,54 @@
 package io.picnicml.doddlemodel.cluster
 
+import breeze.linalg.DenseVector
 import breeze.linalg.functions.euclideanDistance
 import cats.syntax.option._
 import io.picnicml.doddlemodel.data.Features
-import io.picnicml.doddlemodel.typeclasses.Clusterer
 import io.picnicml.doddlemodel.syntax.OptionSyntax._
+import io.picnicml.doddlemodel.typeclasses.Clusterer
 
-/** An immutable DBSCAN model.
+import scala.collection.mutable
+
+/** An immutable DBSCAN clustering model.
   *
-  * @param eps: the maximum distance between points in a group
-  * @param minSamples: the minimum number of point in a core group
-  *
-  * Examples:
-  * val model = DBSCAN()
-  * val model = DBSCAN(eps = 1.5)
-  * val model = DBSCAN(minSamples = 3)
-  * val model = DBSCAN(eps = 2.0, minSamples = 3)
+  * @param eps: the maximum distance between two datapoints to be considered in a common neighborhood
+  * @param minSamples: the minimum number of datapoints in a neighborhood for a point to be considered the core point
   */
-case class DBSCAN private (eps: Double, minSamples: Int, private val labels: Option[Array[Int]])
+case class DBSCAN private (eps: Double, minSamples: Int, private val labels: Option[DenseVector[Double]])
 
 object DBSCAN {
 
-  val NOISE: Int = -1
-  val UNASSIGNED: Int = Int.MaxValue
-
-  def apply(eps: Double = 1.0, minSamples: Int = 1): DBSCAN = {
-    require(eps > 0.0, "Maximum distance needs to be larger than 0")
+  def apply(eps: Double = 0.5, minSamples: Int = 5): DBSCAN = {
+    require(eps > 0.0, "Maximum distance eps needs to be larger than 0")
     require(minSamples > 0, "Minimum number of samples needs to be larger than 0")
     DBSCAN(eps, minSamples, none)
   }
 
   implicit lazy val ev: Clusterer[DBSCAN] = new Clusterer[DBSCAN] {
 
-    override def isFitted(model: DBSCAN): Boolean = model.labels.isDefined
-
-    override protected def labelsSafe(model: DBSCAN): Array[Int] = model.labels.getOrBreak
-
     override protected def copy(model: DBSCAN): DBSCAN =
       model.copy()
 
+    override def isFitted(model: DBSCAN): Boolean = model.labels.isDefined
+
     override protected def fitSafe(model: DBSCAN, x: Features): DBSCAN = {
-      val xSize = x.rows
-      val distanceMap = (0 until xSize - 1).flatMap { i1 =>
-        (i1 + 1 until xSize).map { i2 =>
-          (i1, i2) -> euclideanDistance(x(i1, ::).t, x(i2, ::).t)
-        }
-      }.toMap
-      val labels = Array.fill[Int](xSize)(UNASSIGNED)
-      var groupId = 0
-      for (pointId <- 0 until xSize if labels(pointId) == UNASSIGNED) {
-        var groupQueue = findNeighbors(pointId, distanceMap, xSize, model.eps)
-        if (groupQueue.size + 1 < model.minSamples) {
-          labels(pointId) = NOISE
-        } else {
-          labels(pointId) = groupId
-          while (groupQueue.nonEmpty) {
-            val tmpGroupQueue = groupQueue
-            groupQueue = Set[Int]()
-            tmpGroupQueue.foreach { i =>
-              if (labels(i) == NOISE) labels(i) = groupId
-              else if (labels(i) == UNASSIGNED) {
-                labels(i) = groupId
-                val neighbors = findNeighbors(i, distanceMap, xSize, model.eps)
-                if (neighbors.size + 1 >= model.minSamples)
-                  groupQueue ++= neighbors
-              }
-            }
-          }
-          groupId += 1
-        }
-      }
-      model.copy(labels = labels.some)
+      val distances = computeDistances(x)
+      println(distances)
+      ???
     }
 
-    private def findNeighbors(
-      pointId: Int,
-      distanceMap: Map[(Int, Int), Double],
-      xSize: Int,
-      eps: Double
-    ): Set[Int] = {
-      def findDistance(i1: Int, i2: Int): Double = distanceMap(
-        if (i1 < i2) (i1, i2) else (i2, i1)
-      )
-      (0 until xSize).filter { i =>
-        i != pointId && findDistance(i, pointId) <= eps
-      }.toSet
+    private def computeDistances(x: Features): Distances = {
+      val distanceMatrix = mutable.AnyRefMap[(Int, Int), Double]()
+      (0 until x.rows).combinations(2).foreach { case rowIndex0 +: rowIndex1 +: IndexedSeq() =>
+        distanceMatrix((rowIndex0, rowIndex1)) = euclideanDistance(x(rowIndex0, ::).t, x(rowIndex1, ::).t)
+      }
+      new Distances(distanceMatrix)
     }
+
+    override protected def labelsSafe(model: DBSCAN): DenseVector[Double] = model.labels.getOrBreak
+  }
+
+  private class Distances(private val distanceMatrix: mutable.AnyRefMap[(Int, Int), Double]) {
+    def get(x: Int, y: Int): Double = if (x > y) distanceMatrix((y, x)) else distanceMatrix((x, y))
   }
 }
--- src/test/scala/io/picnicml/doddlemodel/cluster/DBSCANTest.scala
+++ src/test/scala/io/picnicml/doddlemodel/cluster/DBSCANTest.scala
@@ -1,6 +1,6 @@
 package io.picnicml.doddlemodel.cluster
 
-import breeze.linalg.DenseMatrix
+import breeze.linalg.{DenseMatrix, DenseVector}
 import io.picnicml.doddlemodel.TestingUtils
 import io.picnicml.doddlemodel.cluster.DBSCAN.ev
 import org.scalactic.{Equality, TolerantNumerics}
@@ -19,37 +19,36 @@ class DBSCANTest extends FlatSpec with Matchers with TestingUtils {
     List(9.0, 0.0)
   )
 
-  "DBSCAN" should "calculate the label of the data points" in {
+  "DBSCAN" should "cluster the datapoints" in {
     val model = DBSCAN(eps = 3.0, minSamples = 1)
-    ev.fitPredict(model, x) shouldEqual Array(0, 0, 0, 1, 1, 1)
-    ev.labels(ev.fit(model, x)) shouldEqual Array(0, 0, 0, 1, 1, 1)
+    breezeEqual(ev.fitPredict(model, x), DenseVector(0.0, 0.0, 0.0, 1.0, 1.0, 1.0))
   }
 
-  it should "cluster one data point to one group when eps is too small" in {
+  it should "cluster each datapoint into it's own group when eps is too small" in {
     val model = DBSCAN()
-    ev.fitPredict(model, x) shouldEqual Array(0, 1, 2, 3, 4, 5)
+    breezeEqual(ev.fitPredict(model, x), DenseVector(0.0, 1.0, 2.0, 3.0, 4.0, 5.0))
   }
 
-  it should "cluster all data points to one group when eps is too large" in {
+  it should "cluster all data points into a single group when eps is too large" in {
     val model = DBSCAN(eps = 10.0)
-    ev.fitPredict(model, x) shouldEqual Array(0, 0, 0, 0, 0, 0)
+    breezeEqual(ev.fitPredict(model, x), DenseVector(0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
   }
 
-  it should "cluster all points to outliers when min samples is too large" in {
+  it should "label all points as outliers when min samples is too large" in {
     val model = DBSCAN(minSamples = 7)
-    ev.fitPredict(model, x) shouldEqual Array(-1, -1, -1, -1, -1, -1)
+    breezeEqual(ev.fitPredict(model, x), DenseVector(-1.0, -1.0, -1.0, -1.0, -1.0, -1.0))
   }
 
-  it should "cluster all data points to one group when eps is equal to the distance among points" in {
+  it should "cluster all datapoints into a single group when eps equals the distance between points" in {
     val smallX = DenseMatrix(
       List(0.0, 0.0),
       List(3.0, 0.0)
     )
     val model = DBSCAN(eps = 3.0)
-    ev.fitPredict(model, smallX) shouldEqual Array(0, 0)
+    breezeEqual(ev.fitPredict(model, smallX), DenseVector(0.0, 0.0))
   }
 
-  it should "cluster all data points to one group in an 1D array of points that match min sample size" in {
+  it should "cluster all datapoints into a single group" in {
     val d1X = DenseMatrix(
       List(0.0, 12.0),
       List(0.0, 9.0),
@@ -58,7 +57,7 @@ class DBSCANTest extends FlatSpec with Matchers with TestingUtils {
       List(0.0, 0.0)
     )
     val model = DBSCAN(eps = 3.0, minSamples = 3)
-    ev.fitPredict(model, d1X) shouldEqual Array(0, 0, 0, 0, 0)
+    breezeEqual(ev.fitPredict(model, d1X), DenseVector(0.0, 0.0, 0.0, 0.0, 0.0))
   }
 
   it should "prevent the usage of negative eps" in {
